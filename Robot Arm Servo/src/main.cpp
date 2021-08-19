5c3d992f4bbcb3cd88afb44fdc6e3674adfd0f04
AdinAck <adinackerman@gmail.com>
2021-08-19 11:43:31 -0700

    servo moving
    Co-Authored-By: Mew463 <72902803+Mew463@users.noreply.github.com>

--- Robot Arm Servo/src/main.cpp
+++ Robot Arm Servo/src/main.cpp
@@ -1,38 +1,17 @@
 #include <Arduino.h>
 #include <Servo.h>
-Servo myservo; // declare servo
-int integerData;
-String comdata;
+
+Servo servo;
 
 void setup ()
 {
+  servo.attach(3);
   Serial.begin(9600);
-  myservo.attach(3);
-
 }
 
 void loop ()
 {
-  SerialChecker();
-  
-  myservo.write(integerData);
-  
-  delay(50);
-
-}
-
-void SerialChecker()
-{
-  //read string from serial monitor
-  if (Serial.available() > 0) // if we get a valid byte, read analog ins:
-  {
-    comdata = ""; //reset the old values
-    while (Serial.available() > 0)
-    {
-      comdata += char(Serial.read()); //Add the character value to comdata
-      delay(2);//so that whole message is sent before the arduino reads it
-      integerData = comdata.toInt(); //converts our character data into a number
-    }
-    Serial.println(comdata);//prints out what you are typing
+  if (Serial.available() > 0) {
+    servo.write(Serial.readStringUntil('\n').toInt());
   }
 }
