481e2613d0168bbe02c62e31fb7fd7549cf2c2d2
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2021-10-29 10:54:40 -0700

    Improve parameters
    Co-Authored-By: AdinAck <adinackerman@gmail.com>

--- widgets/_data_collection.py
+++ widgets/_data_collection.py
@@ -1,6 +1,6 @@
 from re import L
-from time import time, sleep
-import pickle
+from time import time
+
 from random import randint
 from math import cos, sin
 from itertools import chain
@@ -67,10 +67,10 @@ class Trainer(Widget):
         for motor in self.control._system.motors.values():
             motor.move(0)
 
-        params = {'batch_size': 64, 'gamma': 0.9, 'device': 'cuda', 'double': True,
+        params = {'batch_size': 64, 'gamma': 0.85, 'device': 'cuda', 'double': True,
                   'lr': 0.01, 'target_update': 3000, 'buffer_size': 10000, 'alpha': 0.6, 'beta': 0.4, 'replay_initial': 1000,
-                  'epsilon_start': 1.0, 'epsilon_final': 0.01, 'epsilon_steps': 30000, 'torque_limit': 3.0, 'n_choices': 21, 'step_time': .1,
-                  'new_target_time': 2.0, 'n_reps':10}  # n_choices must be odd
+                  'epsilon_start': 1.0, 'epsilon_final': 0.01, 'epsilon_steps': 60000, 'torque_limit': 3.0, 'n_choices': 5, 'step_time': .1,
+                  'new_target_time': 3.0, 'n_reps': 2}  # n_choices must be odd
 
         def get_torque(action):
             # First normalize action to [-1, 1]
@@ -80,8 +80,8 @@ class Trainer(Widget):
 
         buffer = PriorityBuffer(params['buffer_size'], params['alpha'])
 
-        net = DQN(4, params['n_choices']**2).to(params['device']).to(params['device'])
-        tgt_net = net
+        net = DQN(4, params['n_choices']**2).to(params['device'])
+        tgt_net = DQN(4, params['n_choices']**2).to(params['device'])
         optimizer = torch.optim.Adam(net.parameters(), lr=params['lr'])
         step = 0
 
@@ -91,15 +91,17 @@ class Trainer(Widget):
 
         target: list[float] = [0, 0]  # [m2p, m3p]
         out: list[float] = [0, 0]  # [m2t, m3t]
+        new_target: bool = False
 
         while True:
+            
+            self.drawArms(self.tar_l1, self.tar_l2, target[0], target[1])
+            self.drawArms(self.curr_l1, self.curr_l2, self.control._system.m2.position,
+                            self.control._system.m3.position, out)    
             if time() - frame_start_time < params['step_time']:
-                self.drawArms(self.tar_l1, self.tar_l2, target[0], target[1])
-                self.drawArms(self.curr_l1, self.curr_l2, self.control._system.m2.position,
-                              self.control._system.m3.position, out)
                 continue
 
-            self.fpsVar.set(round(1 / (time() - frame_start_time)))
+            self.fpsVar.set(round(1 / (time() - frame_start_time), 2))
             frame_start_time = time()
 
 
@@ -113,6 +115,7 @@ class Trainer(Widget):
                 target = [t1, t2]  # [m2p, m3p]
                 out = [0, 0]  # [m2t, m3t]
                 new_position_start_time = time()
+                new_target = True
 
             last_state = current_state
             current_state = np.fromiter(
@@ -123,22 +126,32 @@ class Trainer(Widget):
             action_id = choose_action(
                 net(torch.tensor(current_state, dtype=torch.float32).view(1, -1).to(params['device'])), epsilon)
             m2_action, m3_action = divmod(action_id, params['n_choices'])
-            self.control._system.m2.move(get_torque(m2_action))
-            self.control._system.m3.move(get_torque(m3_action))
+            for i, (motor, val) in enumerate(zip([self.control._system.m2, self.control._system.m3], 
+                (get_torque(m2_action), get_torque(m3_action)))):
+                # no applying torque toward out of bounds
+                threshold = 1.5 if i == 0 else 2.2
+                if abs(val) > threshold and val * motor.position > 0:
+                    val = val / abs(val) # sign
+                val = clamp(val, -3, 3)
+                val = round(val, 2)
+                motor.move(val)
+                out[i] = val
 
-            
-
-            if last_state is not None:
+            if last_state is not None and not new_target:
                 reward = -(sum(abs(t - r) for t, r in zip(target,
                            current_state))) * (1 - params['gamma'])
                 experience = Experience(
                     last_state, action_id, reward, current_state, False)
                 buffer.push(experience)
+            new_target = False
 
             # Start training
             if len(buffer) < params['replay_initial']:
                 continue
             for _ in range(params['n_reps']):
+                self.drawArms(self.tar_l1, self.tar_l2, target[0], target[1])
+                self.drawArms(self.curr_l1, self.curr_l2, self.control._system.m2.position,
+                            self.control._system.m3.position, out)   
                 batch, batch_indices, batch_weights = buffer.sample(
                     params['batch_size'], beta=params['beta'])
                 
@@ -156,7 +169,7 @@ class Trainer(Widget):
             if step % params['target_update'] == 0:
                 # save model
                 torch.save(net, 'model.pt')
-                tgt_net = net
+                tgt_net.load_state_dict(net.state_dict())
 
 
     def drawArms(self, line1, line2, t1, t2, torques=None):
