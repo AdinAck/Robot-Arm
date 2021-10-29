50b6f74d1e8eeeea1a16adf035ed0508790750a0
Daniel Monroe <github.com@danielmonroe.net>
2021-10-28 18:21:18 -0700

    use deepq

---
+++ lib/mllib/common.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+import torch
+import numpy as np
+from collections import namedtuple
+
+Experience = namedtuple('Experience', ('state', 'action', 'reward', 'next_state', 'done'))
+
+
+class PriorityBuffer:
+    def __init__(self, capacity, alpha=0.6, max_priority=1):
+        self.capacity = capacity
+        self.buffer = []
+        self.max_priority = max_priority
+        self.position = 0
+        self.alpha = alpha
+        self.priorities = np.zeros(capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def push(self, experience, priority=None):
+        if priority is None:
+            priority = self.max_priority
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = experience
+        self.priorities[self.position] = priority
+        self.position = (self.position + 1) % self.capacity
+
+    
+    def sample(self, batch_size, beta=0.4):
+        if len(self.buffer) == self.capacity:
+            prios = self.priorities
+        else:
+            prios = self.priorities[:self.position]
+        probs = prios ** self.alpha
+        probs /= probs.sum()
+        indices = np.random.choice(len(self.buffer), batch_size, p=probs)
+        samples = [self.buffer[idx] for idx in indices]
+        total = len(self.buffer)
+        weights = (total * probs[indices]) ** (-beta)
+        weights /= weights.max()
+        weights = np.array(weights, dtype=np.float32)
+        return samples, indices, weights
+
+    def update_priorities(self, batch_indices, batch_priorities):
+        for idx, prio in zip(batch_indices, batch_priorities):
+            prio = min(self.max_priority, prio)
+            self.priorities[idx] = prio
+        
+class DQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DQN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_shape, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+        )
+        self.value_head = nn.Linear(128, 1)
+        self.advantage_head = nn.Linear(128, n_actions)
+    
+    def forward(self, x):
+        x = self.model(x)
+        values = self.value_head(x)
+        advantages = self.advantage_head(x)
+        # fix to include values!
+        return advantages
+
+def unpack_batch(batch):
+    states, actions, rewards, dones, next_states = [], [], [], [], []
+    for exp in batch:
+        state = np.array(exp.state, copy=False)
+        states.append(state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.next_state is None)
+        if exp.next_state is None:
+            next_states.append(state)       # the result will be masked anyway
+        else:
+            next_states.append(np.array(exp.next_state, copy=False))
+    return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
+           np.array(dones, dtype=np.uint8), np.array(next_states, copy=False)
+
+def calc_losses(batch, net, tgt_net, gamma, device='cpu', double=True):
+    states, actions, rewards, dones, next_states = unpack_batch(batch)
+
+    states = torch.FloatTensor(states).to(device)
+    next_states = torch.FloatTensor(next_states).to(device)
+    actions = torch.LongTensor(actions).to(device)
+    rewards = torch.FloatTensor(rewards).to(device)
+    dones = torch.ByteTensor(dones).to(device)
+
+    state_action_values = net(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)
+    if double:
+        next_state_actions = net(next_states).max(1)[1]
+        next_state_values = tgt_net(next_states).gather(1, next_state_actions.unsqueeze(-1)).squeeze(-1)
+    else:
+        next_state_values = tgt_net(next_states).max(1)[0]
+    next_state_values[dones] = 0.0
+
+    expected_state_action_values = next_state_values.detach() * gamma + rewards
+
+    return (state_action_values - expected_state_action_values) ** 2 / 2
+
+def choose_action(advantages, epsilon=0.01):
+    # epsilon-greedy
+    if np.random.random() < epsilon:
+        return np.random.randint(0, len(advantages))
+    else:
+        return advantages.argmax()
--- widgets/_data_collection.py
+++ widgets/_data_collection.py
@@ -5,76 +5,19 @@ from random import randint
 from math import cos, sin, sqrt
 from itertools import chain
 from typing import Callable
+import torch
 import logging
 
 from lib.app import Application
 import tkinter as tk
 
 import neat
-import visualize
+from lib.mllib.common import *
 from lib.utils import *
 from lib.widget import Widget
 
 
-class Model:
-    def __init__(self, eval, config, checkpoint=None):
-        self.externalEval = eval
-        self.config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                             neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                             config)
-                        
-        # Create the population, which is the top-level object for a NEAT run.
-        if checkpoint is not None:
-            self.population = neat.Checkpointer.restore_checkpoint(checkpoint)
-        else:
-            self.population = neat.Population(self.config)
-        # Add a stdout reporter to show progress in the terminal.
-        self.population.add_reporter(neat.StdOutReporter(True))
-        self.stats = neat.StatisticsReporter()
-        self.population.add_reporter(self.stats)
-        self.population.add_reporter(neat.Checkpointer(1))
-
-
-    def eval_genomes(self, genomes, config):
-        for _, genome in genomes:
-            net = neat.nn.FeedForwardNetwork.create(genome, config)
-            genome.fitness = self.externalEval(net)
-
-    def train(self, generations=100) -> neat.nn.FeedForwardNetwork:
-
-        # Run for specified number of generations.
-        winner = self.population.run(self.eval_genomes, generations)
-
-        # Display the winning genome.
-        print('\nBest genome:\n{!s}'.format(winner))
-
-        # Show output of the most fit genome against training data.
-        print('\nOutput:')
-        winner_net = neat.nn.FeedForwardNetwork.create(winner, self.config)
-
-        #!!!
-        inputs = ['M2 pos target', 'M3 pos target', 'M2 pos current', 'M2 vel current', 'M3 pos current', 'M3 vel current']
-        outputs = ['M2 Torque', 'M3 Torque']
-        node_names = {-(i+1): inputs[i] for i in range(len(inputs))} | {i: outputs[i] for i in range(len(outputs))}
-
-        #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-50')
-        winner = self.population.run(self.eval_genomes, generations)
-
-        visualize.draw_net(self.config, winner, node_names=node_names)
-        visualize.plot_stats(self.stats, ylog=False)
-        visualize.plot_species(self.stats)
-
-        with open('winner.net', 'wb') as f:
-            pickle.dump(winner_net, f)
-
-        return winner_net
-
-    def predict(self) -> list:
-        return []
-
-
 class Trainer(Widget):
-    model: Model
     def __init__(self, root, config):
 
         self.config = config
@@ -113,7 +56,7 @@ class Trainer(Widget):
     
     
     def run(self):
-        self.model = Model(self.runModel, self.config.neat_config, self.config.checkpoint)
+        self.model = DQN(self.runModel, self.config.neat_config, self.config.checkpoint)
         self.model.train(self.config.generations)
         for motor in self.control._system.motors.values():
             motor.disable()
@@ -135,61 +78,76 @@ class Trainer(Widget):
             self.canvas.itemconfig(line2, width=2*abs(torques[1])+0.1, fill=('green' if torques[1] > 0 else 'red'))
 
 
-    def runModel(self, net: neat.nn.FeedForwardNetwork):
+    def runModel(self):
         # logging.getLogger().setLevel(logging.DEBUG)
         for motor in self.control._system.motors.values():
             motor.move(0)
         
+        params = {'batch_size': 64, 'gamma': 0.9, 'device': 'cpu', 'double': True, 
+        'lr': 0.01, 'target_update': 1000, 'buffer_size': 10000, 'alpha': 0.6, 'beta': 0.4, 'replay_initial': 1000,
+        'epsilon_start': 1.0, 'epsilon_final': 0.01, 'epsilon_steps': 1000, 'torque_limit': 3.0, 'n_choices': 21, 'step_time': .1} # n_choices must be odd
+        buffer = PriorityBuffer(params['buffer_size'], params['alpha'])
+        
+        net = DQN(4, params['n_choices']**2)
+        tgt_net = net
+        optimizer = torch.optim.Adam(net.parameters(), lr=params['lr'])
+        step = 0
+        
+        t = randint(-157, 157)/100
+        r = randint(10, 30)
+        self.control.x = r*cos(t)
+        self.control.y = r*sin(t)
         t1, t2 = self.control._system.cartesianToDualPolar(
-        self.control.x, self.control.y)
+            self.control.x, self.control.y)
         target: list[float] = [t1, t2]  # [m2p, m3p]
         out: list[float] = [0, 0]  # [m2t, m3t]
-        
+        current_state = None
+        start_time = -100
+        while True:
+            if time() - start_time < params['step_time']:
+                continue
+            start_time = time()
+            last_state = current_state
+            current_state = np.fromiter(attr() for attr in self.attrs)
 
-        total_error = 0
-        n_steps = 0
-        for trial in range(self.config.trials):
-            time_start = time()
-            
-            t = randint(-157, 157)/100
-            r = randint(10, 30)
-            self.control.x = r*cos(t)
-            self.control.y = r*sin(t)
-            t1, t2 = self.control._system.cartesianToDualPolar(
-                self.control.x, self.control.y)
-            target: list[float] = [t1, t2]  # [m2p, m3p]
-            out: list[float] = [0, 0]  # [m2t, m3t]
-            while (countdown := self.config.duration - (time() - time_start)) >= 0:
-                current = [attr() for attr in self.attrs]
-                single_error = sum(abs(t - r) for t, r in zip(target, current))
-                # !!! [countdown] +, abs and not ** above here
-                out = net.activate(list(chain(target, current)))
-                current_x, current_y = self.control._system.dualPolarToCartesian(out[0], out[1])
-                fake_error = sqrt((current_x - self.control.x)**2 + (current_y - self.control.y)**2)
-                #out = [target[0] - current[0], target[1] - current[1]]
-                for i, (motor, val) in enumerate(zip([self.control._system.m2, self.control._system.m3], out)):
-
-                    # no applying torque toward out of bounds
-                    threshold = 1.5 if i == 0 else 2.2
-                    if abs(val) > threshold and val * motor.position > 0:
-                        single_error += 2
-                        val = 0
-                    val *= 2.5
-                    val = clamp(val, -3, 3)
-                    val = round(val, 2)
-                    motor.move(val)
+            epsilon = max(params['epsilon_final'], params['epsilon_start'] - step * (params['epsilon_start'] - params['epsilon_final']) / params['epsilon_steps'])
+            action_id = choose_action(net(torch.tensor(current_state, dtype=torch.float32).view(1, -1)), epsilon)
+            m2_action, m3_action = divmod(action_id, params['n_choices'])
+            def get_torque(action):
+                # First normalize action to [-1, 1]
+                action = (action / (params['n_choices'] - 1) / 2) - 1
+                # Then scale to torque limit
+                return action * params['torque_limit']
+            self.control._system.m2.move(get_torque(m2_action))
+            self.control._system.m3.move(get_torque(m3_action))
 
                 
-                self.drawArms(self.tar_l1, self.tar_l2, target[0], target[1])
-                self.drawArms(self.curr_l1, self.curr_l2, self.control._system.m2.position,
-                            self.control._system.m3.position, out)
+            self.drawArms(self.tar_l1, self.tar_l2, target[0], target[1])
+            self.drawArms(self.curr_l1, self.curr_l2, self.control._system.m2.position,
+                        self.control._system.m3.position, out)
 
-                
-                n_steps += 1
-                total_error += single_error
-        logging.debug(f'Average error {total_error / n_steps} with {n_steps=}')
-        assert n_steps > 0, 'Cannot have 0 time steps'
-        return -total_error / n_steps  # fitness
+            step += 1
+
+            if last_state is not None:
+                reward = -(sum(abs(t - r) for t, r in zip(target, current_state)))
+                experience = Experience(last_state, action_id, reward, current_state, False)
+                buffer.push(experience)
+
+            # Start training
+            if len(buffer) < params['replay_initial']:
+                continue
+
+            batch, batch_indices, batch_weights = buffer.sample(params['batch_size'], beta=params['beta'])
+            batch_weights = torch.Tensor(batch_weights, device=params['device'])
+            optimizer.zero_grad()
+            losses = calc_losses(batch, net, tgt_net, params['gamma'], params['device'], params['double'])
+            buffer.update_priorities(batch_indices, losses.detach().cpu().numpy())
+
+            torch.sum(losses * batch_weights).backward()
+            optimizer.step()
+            step += 1
+            if step % params['target_update'] == 0:
+                tgt_net = net
 
 class TrainApp(Application):
     def __init__(self, root, train_config):
