4e38d3c23154d8892b2de736b90f049926ca6a0b
Daniel Monroe <39802758+Ergodice@users.noreply.github.com>
2022-02-27 15:51:00 -0800

    Reduced jittering

--- widgets/builtin/hand_tracking.py
+++ widgets/builtin/hand_tracking.py
@@ -44,6 +44,12 @@ def sgn(x):
         return -1
     return 1
 
+def normed_dot(x1, y1, z1, x2, y2, z2):
+    dot = x1 * x2 + y1 * y2 + z1 * z2
+    this_mag = sqrt(x1**2 + y1**2 + z1**2)
+    last_mag = sqrt(x2**2 + y2**2 + z2**2)
+    return dot/this_mag/last_mag
+
 def calc_curve(landmarks):
     vectors = []
     last_lm = None
@@ -59,11 +65,7 @@ def calc_curve(landmarks):
         for j in range(i + 1, len(vectors)):
             x1, y1, z1 = vectors[i]
             x2, y2, z2 = vectors[j]
-            dot = x1 * x2 + y1 * y2 + z1 * z2
-            this_mag = sqrt(x1**2 + y1**2 + z1**2)
-            last_mag = sqrt(x2**2 + y2**2 + z2**2)
-            fixed_dot = dot/this_mag/last_mag
-            dot_total += fixed_dot
+            dot_total += normed_dot(x1, y1, z1, x2, y2, z2)
     return dot_total / (len(landmarks) - 1)
 
 
@@ -73,7 +75,15 @@ class HandTracking(Widget):
         Thread(target=self.main, daemon=True).start()
 
     def main(self):
+        num_attrs = 4
+        avgs = [None] * num_attrs
+        avg_factors = [.5] * num_attrs
+        last_moves = [0] * num_attrs
+        master_pos = [-999] * num_attrs
+        sensitivity = [.15, .3, .65, 10] * num_attrs
+
         x_avg, y_avg, z_avg = None, None, None
+        curve_avg = None
         x_vel_sgn_count = 0
         x_vel_sgn = 0
         y_vel_sgn_count = 0
@@ -81,13 +91,21 @@ class HandTracking(Widget):
         x_vel_avg, y_vel_avg = 0, 0
         factor = .5
         start = time()
+        hand_on = False
+        hand_on_start = time()
+        hand_on_bound = 1
         while True:
+            
             _, img = cap.read()
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             results = hands.process(imgRGB)
             # print("[INFO] handmarks: {}".format(results.multi_hand_landmarks))
-
+            
             if results.multi_hand_landmarks:
+                if not hand_on:
+                    hand_on = True
+                    hand_on_start = time()
+
                 for hand_landmarks in results.multi_hand_landmarks:
                     index = 0
                     for lm in hand_landmarks.landmark:
@@ -100,62 +118,68 @@ class HandTracking(Widget):
                 #print(results.multi_hand_landmarks)
                 landmarks = results.multi_hand_landmarks[0].landmark
 
-                
-                avgs = calc_avgs([0, 5, 9, 13, 17], landmarks)
-                x = avgs[0]
-                y = avgs[1]
-                lm1 = landmarks[0]
-                def calc_dist(lm1, lm2):
-                    return sqrt((lm1.x - lm2.x)**2 + (lm1.y - lm2.y)**2 + (lm1.z - lm2.z)**2)
-                z = calc_dist(lm1, landmarks[5]) + calc_dist(lm1, landmarks[17])
-                curve = sum(calc_curve(landmarks[4*i + 5: 4*i+9]) for i in range(4)) / 4
+                 
+                if time() - hand_on_start > hand_on_bound:
+                    x, y, *_ = calc_avgs([0, 5, 9, 13, 17], landmarks)
+                    lm1 = landmarks[0]
+                    def calc_dist(lm1, lm2):
+                        return sqrt((lm1.x - lm2.x)**2 + (lm1.y - lm2.y)**2 + (lm1.z - lm2.z)**2)
+                    z = calc_dist(lm1, landmarks[5]) + calc_dist(lm1, landmarks[17])
+                    curve = sum(calc_curve(landmarks[4*i + 5: 4*i+9]) for i in range(4)) / 4
+                    thumb_curve = calc_curve([landmarks[0], landmarks[1], landmarks[2]])
+                    print(curve)
+                    #print(thumb_curve)
+                    full_pos = [x, y, z, curve] #this is the position in the latent space
+                    full_pos = [clip(1/z, 2, 5, 0, 30), clip(x, .1, .95, -30, 30),
+                        clip(y, .1, .85, 10, 140), clip(curve, -.3, 1, 0, 100)]
 
-
-                if x_avg is None:
-                    x_avg = x
-                    y_avg = y
-                    z_avg = z
-                else:
-                    '''
-                    other_factor = .3
-                    x_vel = x - x_avg
-                    y_vel = y - y_avg
-                
-                    x_vel_avg = x_vel * factor + x_vel_avg * (1- other_factor)
-                    y_vel_avg = y_vel * factor + y_vel_avg * (1- other_factor)
-                    if sgn(x_vel) == x_vel_sgn:
-                        x_vel_sgn_count += 1
+                    if avgs[0] is None:
+                        avgs = full_pos
                     else:
-                        x_vel_avg = 0
-                        x_vel_sgn_count = 0
+                        '''
+                        other_factor = .3
+                        x_vel = x - x_avg
+                        y_vel = y - y_avg
+                    
+                        x_vel_avg = x_vel * factor + x_vel_avg * (1- other_factor)
+                        y_vel_avg = y_vel * factor + y_vel_avg * (1- other_factor)
+                        if sgn(x_vel) == x_vel_sgn:
+                            x_vel_sgn_count += 1
+                        else:
+                            x_vel_avg = 0
+                            x_vel_sgn_count = 0
 
-                    if sgn(y_vel) == y_vel_sgn:
-                        y_vel_sgn_count += 1
-                    else:
-                        y_vel_avg = 0
-                        y_vel_sgn_count = 0
-                    '''
+                        if sgn(y_vel) == y_vel_sgn:
+                            y_vel_sgn_count += 1
+                        else:
+                            y_vel_avg = 0
+                            y_vel_sgn_count = 0
+                        '''
+                        for i in range(num_attrs):
+                            avgs[i] = avgs[i] * avg_factors[i] + full_pos[i] * (1-avg_factors[i])
 
-                    x_avg = x * factor + x_avg * (1 - factor)
-                    y_avg = y * factor + y_avg * (1 - factor)
-                    z_avg = z * factor + z_avg * (1 - factor)
+                    #lookahead=0
+                    #use_x, use_y, use_z = x_avg + x_vel_avg*lookahead, y_avg+y_vel_avg*lookahead, z_avg
+                    
+                    
+                    for i in range(num_attrs):
+                        if abs(master_pos[i] - avgs[i]) > sensitivity[i]:
+                            last_moves[i] = time()
+                            master_pos[i] = avgs[i]
 
-                #lookahead=0
-                #use_x, use_y, use_z = x_avg + x_vel_avg*lookahead, y_avg+y_vel_avg*lookahead, z_avg
-                
-                #print(curve)
-                pos = [clip(1/z_avg, 2, 5, 0, 30), clip(x_avg, .1, .95, -30, 30), clip(y_avg, .1, .85, 10, 140)]
-                curve = clip(curve, .2, 1, 100, 0)
-                #pos = [clip(z_avg, .1, .05, 0, 30), clip(x_avg, .1, .95, -30, 30), clip(y_avg, 0, 1, 10, 140)]
-                #print(30*(1-point.y), 60*(.5-point.x), point.z)
-                #pos = ','.join(map(str, pos))
-                try:
-                    self.control.move(x=pos[0], y=pos[1], z=pos[2], e=curve)
-                except:
-                    print('OOF')
-                # sleep(.025)
-                #print(time() - start)
-                start = time()
+                    #master_pos = full_pos
+                    
+
+                    #pos = [clip(z_avg, .1, .05, 0, 30), clip(x_avg, .1, .95, -30, 30), clip(y_avg, 0, 1, 10, 140)]
+                    #print(30*(1-point.y), 60*(.5-point.x), point.z)
+                    #pos = ','.join(map(str, pos))
+                    try:
+                        self.control.move(x=master_pos[0], y=master_pos[1],
+                            z=master_pos[2], e=master_pos[3])
+                    except:
+                        print('OOF')
+                    # sleep(.025)
+                    #print(time() - start)
             img = cv2.flip(img, 1)
             cv2.imshow("Image", img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
