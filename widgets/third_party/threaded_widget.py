7c62d1725b917f9bc8edfb6ab0a079eac6915c53
Adin Ackerman <adinackerman@gmail.com>
2022-07-15 21:44:19 -0700

    docs...

--- widgets/third_party/threaded_widget.py
+++ widgets/third_party/threaded_widget.py
@@ -2,12 +2,12 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 from lib.widget import Widget
+from lib.utils import threaded_callback
 
 
-class MyWidget(Widget):
+class ThreadedWidget(Widget):
     # our setup function creates and integrates some UI elements
     def setup(self):
-        # the parent of our elements is 'self' because our class is a tk window
         ttk.Label(self, text='Hello, World!').pack( # create and pack a label
             padx=10, pady=10
         )
@@ -16,9 +16,16 @@ class MyWidget(Widget):
             self,
             text='Button',
             command=self._callback # and bind it to a callback
-        )
+        )                     
         self.button.pack(padx=10, pady=10) # pack button
 
-    # just a regular old function that we want the button to execute
+    # a callback function that is executed in a separate thread
+    @threaded_callback
     def _callback(self):
+        self.button.config(text='Working...') # change the button text
+        self.button.config(state='disabled')  # disable the button
+
         self.control.move('smooth', x=0, y=20) # move the arm to 0, 20, z=0, r=0, e=0
+
+        self.button.config(state='normal') # enable the button
+        self.button.config(text='Button')  # change the button text back
